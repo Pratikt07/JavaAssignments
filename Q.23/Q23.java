d3f02ab8c8b55115d4c57a8f9eb2d64f778a8e1c
Pratikt07 <Pratikt9999@gmail.com>
2020-09-19 01:30:25 +0530

    Second Day

--- Q.23/Q23.java
+++ Q.23/Q23.java
@@ -4,6 +4,7 @@ class Q23{
 		Scanner sc = new Scanner(System.in);
 		int a[] = new int [5];
 		int b[] = new int[5];
+		int n = a.length-1;
 		for(int i =0,j=a.length-1;i<a.length;i++,j--)
 		{
 			 a[i] = sc.nextInt();
@@ -19,5 +20,15 @@ class Q23{
 		System.out.print(p+" ");
 		}
 		System.out.println(" ");
+		for(int k=0;k<=n/2;k++){
+			int temp = a[n-k];
+			a[n-k] = a[k];
+			a[k] = temp;
+			 	
+		}
+		System.out.println("Reversed Array :  ");
+		for(int q : a){
+		System.out.print(q+" ");
+		}
  	}
 }
